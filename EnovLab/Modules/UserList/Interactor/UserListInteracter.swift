432312af0cfb974f127824c0fbe696195d6ab57e
Charles Graffeo <chgraffeo@gmail.com>
2017-12-01 03:58:37 -0500

    Gets data from json file and converts it to array of users

--- EnovLab/Modules/UserList/Interactor/UserListInteracter.swift
+++ EnovLab/Modules/UserList/Interactor/UserListInteracter.swift
@@ -9,11 +9,64 @@
 import UIKit
 
 class UserListInteracter: UsersListCase {
+    
     var output: UserListInteracterOutput!
     
+    // MARK: User List Case
     func fetchUsers() {
+        
+        if let jsonArray = self.userListData(){
+            
+            let userList = self.convertJsonToUsers(userList: jsonArray)
+            self.output.usersFetched(userList)
+            
+        }
+        else{
+            
+        }
         
     }
     
-
+    // MARK: Private
+    
+    private func userListFilepath() -> URL?{
+        return Bundle.main.url(forResource: "users", withExtension: "json")
+    }
+    
+    private func userListData()->Array<Dictionary<String,AnyObject>>?{
+        
+        do{
+            let data = try Data(contentsOf: self.userListFilepath()!)
+            let jsonArray = try JSONSerialization.jsonObject(with: data, options:[])
+            return jsonArray as? Array<Dictionary<String,AnyObject>>
+        }
+        catch(let error){
+            print(error.localizedDescription)
+            return nil
+        }
+        
+    }
+    
+    private func convertJsonToUsers(userList:Array<Dictionary<String,AnyObject>>) -> [User]{
+        
+        var users = [User]()
+        for user in userList{
+            
+            let u = User(id: user["id"] as! Int,
+                         firstName: user["first_name"] as! String,
+                         lastName: user["last_name"] as! String,
+                         email: user["email"]  as! String,
+                         gender: user["gender"]  as! String,
+                         avatarUrl: user["avatar"]  as! String,
+                         jobTitle: user["job_title"]  as! String,
+                         age: user["age"]  as! Int,
+                         description: user["desc"]  as! String)
+            
+            users.append(u)
+            
+        }
+        return users
+        
+    }
+    
 }
