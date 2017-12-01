101b3a3bc0413c48fc5ecd04d8a957a554d95ba9
Charles Graffeo <chgraffeo@gmail.com>
2017-12-01 03:57:42 -0500

    Added initalizer to user model

--- EnovLab/Models/User.swift
+++ EnovLab/Models/User.swift
@@ -10,4 +10,26 @@ import UIKit
 
 class User {
 
+    let id:Int!
+    let firstName:String!
+    let lastName:String!
+    let email:String!
+    let gender:String!
+    let avatarUrl:String!
+    let jobTitle:String!
+    let age:Int!
+    let description:String!
+    
+    init(id:Int, firstName:String, lastName:String,email:String,gender:String,avatarUrl:String,jobTitle:String,age:Int,description:String) {
+        
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.gender = gender
+        self.avatarUrl = avatarUrl
+        self.jobTitle = jobTitle
+        self.age = age
+        self.description = description
+    }
 }
